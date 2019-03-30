7d26e5000929310152713c92831f20be1c6eb77a
osagieomigie <osagieomigie@gmail.com>
2019-03-30 15:56:36 -0400

    Able to evaluate expressions now

--- cal.js
+++ cal.js
@@ -1,39 +1,39 @@
 
 var button; 
-var screen; 
+var calScreen; 
 var result = 0;
-//var equalButton []; 
 
 button = document.querySelectorAll(".btn");
-screen = document.querySelector("#tsx");
+calScreen = document.querySelector("#tsx");
 
 
 for(var i = 0; i<button.length; i++){
 	button[i].addEventListener("click", function (button){
-		//alert("you pressed " + this.innerText +"!!");
-		//equalButton.push() should append to array
 
-		if(screen.innerText === "0"){
-			screen.innerText = "";
+		if(calScreen.innerText === "0"){ // p 0  
+			calScreen.innerText = "";
 		}
 
 		if(this.innerText === "AC"){
-			screen.innerText = "0";
+			calScreen.innerText = "0";
 		}
 		else{
-			screen.innerText += this.innerText;
+
+			if(this.innerText != "="){
+				calScreen.innerText += this.innerText;
+			}
 		}
 		
 		if(this.innerText === "="){
-			screen.innerText = result; 
-			//equalButton.push() Calculate result 
+			if(calScreen.innerText.length >= 3){
+					result = eval(calScreen.innerText);
+					calScreen.innerText = result;
+				} 
 		}
 });
 }
 
 
 
-/*button.forEach(function(aButton){
-	aButton.addEventListener("click", alert("you pressed a button!!"));
-});*/
+
 
