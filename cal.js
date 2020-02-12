f86abfe9d89bf1c64721b1e90436635db917c107
Osagie Omigie <osagieomigie@gmail.com>
2020-02-12 13:23:30 -0700

    Fixed more parentheses bug

--- cal.js
+++ cal.js
@@ -5,14 +5,19 @@ let result = 0;
 let previousResult = 0;
 
 button = document.querySelectorAll(".btn");
-calScreen = document.querySelector("#tsx");
+calScreen = document.querySelector("#resultScreen");
 
 
 for(var i = 0; i<button.length; i++){
 	button[i].addEventListener("click", function (button){
 
-		if(calScreen.innerText === "0"){ // p 0  
+		if(calScreen.innerText === "0"){ 
 			calScreen.innerText = "";
+		}
+
+		// handle CE case here
+		if(calScreen.innerText === "CE"){
+			//do something here 
 		}
 
 		if(this.innerText === "C"){
@@ -26,39 +31,54 @@ for(var i = 0; i<button.length; i++){
 		}
 		
 		if(this.innerText === "="){
-			if(calScreen.innerText.length >= 3){
+			if(calScreen.innerText.length !== 0){
 				let screenContains = calScreen.innerText;
 				try{
 					let tmpBracket = screenContains.indexOf("(");
+					let closingBracket = screenContains.indexOf(")");
 					let tmpBracket_operator =screenContains.charAt(tmpBracket-1);
 
 					// check if expression uses brackets
 					if ((tmpBracket != -1 ) && (tmpBracket_operator != 0)){
+						// check for closing brackets
+						if (closingBracket < 0 ){
+							console.log("No closing bracket");
+							throw "ERROR";
+						}
 
 						if ((tmpBracket_operator !== "+") && (tmpBracket_operator !== "-") && (tmpBracket_operator !== "/") && (tmpBracket_operator !== "*") ){
 							screenContains = screenContains.slice(0, tmpBracket) + "*" + screenContains.slice(tmpBracket);
-							alert(screenContains);
+							//alert(screenContains);
 							result = eval(screenContains);
 							//previousResult = screenContains;
-							calScreen.innerText = result;
+							//calScreen.innerText = result;
 						}else{
-							alert("Has operator");
-							alert(screenContains);
+							//alert("Has operator");
+							//alert(screenContains);
 							result = eval(screenContains);
-							calScreen.innerText = result;
+							//calScreen.innerText = result;
 						}
 					}else{
 						result = eval(screenContains);
-						calScreen.innerText = result;
+						//calScreen.innerText = result;
 					}
 					
 					//previousResult = calScreen.innerText
-					
+
+					// captures the inifinity case from eval
+					if (result == "Infinity"){
+						result = "ERROR";
+					}
+
+					calScreen.innerText = result;
 					console.log("The result: " + result);
 				}catch(error){
 					//prevCalScreen.innerText = previousResult + "=Syntax ERROR";
 					calScreen.innerText = "Syntax ERROR";
 				}
+			}else{
+				calScreen.innerText = "0";
+				// dp the repeating equal sign done in google calculator.
 			} 
 		}
 });
