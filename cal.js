0d93b4735d93408eb199c68ab2cb8f8bfc3995b3
Osagie Omigie <osagieomigie@gmail.com>
2020-02-13 22:48:18 -0700

    Added CE functionality

--- cal.js
+++ cal.js
@@ -2,31 +2,37 @@
 let button; 
 let calScreen; 
 let result = 0;
-let previousResult = 0;
+let previousTmp = 0;
 
 button = document.querySelectorAll(".btn");
 calScreen = document.querySelector("#resultScreen"); 
-previousResult = document.querySelector("#prevResultScreen");
+let previousResult = document.querySelector("#prevResultScreen");
 let screenContains = calScreen.innerText;
 
 for(var i = 0; i<button.length; i++){
 	button[i].addEventListener("click", function (button){
 
+		// clear screen before user starts entering input
 		if(calScreen.innerText === "0"){ 
 			calScreen.innerText = "";
 		}
 
-		// handle CE case here
-		if(screenContains.includes("CE")){
-			//delete last digit from number
-			// let deletePos = calScreen.length(); 
-			// calScreen = calScreen.slice(0, deletePos);
-			// console.log("clearing digit");
-		}
-
+		// clear screen 
 		if(this.innerText === "C"){
 			calScreen.innerText = "0";
-			previousResult.innerText = "Ans = "+result; 
+			previousResult.innerText = "Ans = "+previousTmp; 
+		}
+		else if (this.innerText === "CE"){
+			//delete last digit from number
+			console.log("clearing digit")
+			let tmpSC = calScreen.innerText;
+			let deletePos = tmpSC.length-1; 
+			calScreen.innerText = tmpSC.slice(0, deletePos);
+
+			// handles case when there was only 1 digit on screen 
+			if (calScreen.innerText.length === 0){
+				calScreen.innerText = 0; 
+			}
 		}
 		else{
 			if(this.innerText != "="){
@@ -36,7 +42,9 @@ for(var i = 0; i<button.length; i++){
 		
 		if(this.innerText === "="){
 			if(calScreen.innerText.length !== 0){
+				screenContains = calScreen.innerText;
 				previousResult.innerText = screenContains+"=";
+				previousTmp = screenContains+"=";
 
 				try{
 					let tmpBracket = screenContains.indexOf("(");
@@ -46,7 +54,7 @@ for(var i = 0; i<button.length; i++){
 					// check if expression uses brackets
 					if ((tmpBracket != -1 ) && (tmpBracket_operator != 0)){
 
-						// check for closing brackets
+						// check for closing brackets, throw error if there isnt one 
 						if (closingBracket < 0 ){
 							console.log("No closing bracket");
 							throw "ERROR";
@@ -54,8 +62,9 @@ for(var i = 0; i<button.length; i++){
 
 						if ((tmpBracket_operator !== "+") && (tmpBracket_operator !== "-") && (tmpBracket_operator !== "/") && (tmpBracket_operator !== "*") ){
 							screenContains = screenContains.slice(0, tmpBracket) + "*" + screenContains.slice(tmpBracket);
+							previousResult.innerText = screenContains + "=";
+							previousTmp = screenContains + "=";
 							result = eval(screenContains);
-							//previousResult = screenContains;
 						}else{
 							result = eval(screenContains);
 						}
@@ -69,15 +78,17 @@ for(var i = 0; i<button.length; i++){
 					}
 
 					calScreen.innerText = result;
+					previousTmp = screenContains + "=";
 					previousResult.innerText = screenContains + "=";
 					console.log("The result: " + result);
 				}catch(error){
 					// handle case of when theres an error on the 1st calculation
 					if ( previousResult instanceof HTMLButtonElement){
 						previousResult.innerText = screenContains + "=Syntax ERROR";
-						previousResult = screenContains +  "=Syntax ERROR";
+						previousTmp = screenContains +  "=Syntax ERROR";
 					}else{
 						previousResult.innerText = previousResult + "=Syntax ERROR";
+						previousTmp = screenContains +  "=Syntax ERROR";
 					}
 
 					calScreen.innerText = "Syntax ERROR";
